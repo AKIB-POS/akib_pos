0198720e2335d0a634af4485ae8b8802a84b463a
ASUS <fadilmuhaimin88@gmail.com>
2024-07-27 19:50:19 +0800

    add example feature

---
+++ lib/features/Sample/presentation/pages/Sample_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class SamplePage extends StatefulWidget {
+	@override
+	createState() => _SamplePage();
+}
+class _SamplePage extends State<SamplePage> {
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(),
+		);
+	}
+}
